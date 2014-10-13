7897e8717daf3d47b0cd1336d853ccb71a8185f6
TJ Stankus <tjstankus@gmail.com>
2014-10-13 15:10:28 -0400

    [TS/TC][80582522] Initial commit
    Gem generated with bundler

---
+++ lib/virtus/mapper.rb
@@ -0,0 +1,7 @@
+require "virtus/mapper/version"
+
+module Virtus
+  module Mapper
+    # Your code goes here...
+  end
+end
---
+++ lib/virtus/mapper/version.rb
@@ -0,0 +1,5 @@
+module Virtus
+  module Mapper
+    VERSION = "0.0.1"
+  end
+end
